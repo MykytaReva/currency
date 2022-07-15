b6499a1f9b8b46b513d2ddc1b18a50c1aa780aa8
Mykyta Reva <mykytareva@gmail.com>
2022-07-15 22:36:32 +0200

    Hello world added

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from currency.utils import generate
 
-
+def printer(request):
+    return HttpResponse('Hello World!')
 def generator(request):
     result = generate()
     return HttpResponse(result)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from currency.views import generator
+from currency.views import generator, printer
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('generate/', generator)
+    path('generate/', generator),
+    path('hi/', printer)
 ]
