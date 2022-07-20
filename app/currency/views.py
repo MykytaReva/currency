edadcf47610750a5c4380c97644057d8e23cdd0b
Mykyta Reva <mykytareva@gmail.com>
2022-07-20 20:15:19 +0200

    Models and tables created

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,7 +1,22 @@
-from django.http import HttpResponse
-from currency.utils import generate
+from currency.models import Rate, ContactUs
+from django.shortcuts import render
 
 
-def generator(request):
-    result = generate()
-    return HttpResponse(result)
+def index(request):
+    return render(request, 'index.html')
+
+
+def rate_list(request):
+    context = {
+        'rate_list': Rate.objects.all(),
+    }
+
+    return render(request, 'rate_list.html', context=context)
+
+
+def contact_us(request):
+    context = {
+        'contactus_list': ContactUs.objects.all(),
+    }
+
+    return render(request, 'contact_us.html', context=context)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from currency.views import generator
+from currency.views import rate_list, index, contact_us
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('generate/', generator)
+    path('rate/list/', rate_list),
+    path('contact/us/', contact_us),
+    path('', index),
 ]
