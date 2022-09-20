123f5dca9606880cd566bd0d0788d711b404cddf
Mykyta Reva <mykytareva@gmail.com>
2022-09-20 16:39:35 +0200

    Filters for Source and ContactUs+SearchFilter

--- app/api/v1/filters.py
+++ app/api/v1/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from currency.models import Rate
+from currency.models import Rate, Source, ContactUs
 
 
 class RateFilter(django_filters.FilterSet):
@@ -10,5 +10,22 @@ class RateFilter(django_filters.FilterSet):
             'sale': ('gte', 'lte', 'lt', 'gt'),
             'source': ('exact', ),
             'base_currency_type': ('exact', ),
-            'currency_type': ('exact', ),
+            'currency_type': ('exact', )
+        }
+
+
+class SourceFilter(django_filters.FilterSet):
+    class Meta:
+        model = Source
+        fields = {
+            'name': ('exact', )
+        }
+
+
+class ContactUsFilter(django_filters.FilterSet):
+    class Meta:
+        model = ContactUs
+        fields = {
+            'email_to': ('exact', ),
+            'subject': ('exact', )
         }
--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -10,7 +10,8 @@ from api.v1.serializer import RateSerializer, SourceSerializer, ContactUsSeriali
 
 from api.v1.pagination import RatePagination
 
-from api.v1.filters import RateFilter
+from api.v1.filters import RateFilter, ContactUsFilter, SourceFilter
+
 
 from api.v1.throttles import AnonCurrencyThrottle
 
@@ -31,8 +32,24 @@ class RateViewSet(ModelViewSet):
 class SourceView(generics.ListAPIView):
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
+    filterset_class = SourceFilter
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        rest_framework_filters.OrderingFilter
+    )
+    ordering_field = ['id', 'name']
+    throttle_classes = [AnonCurrencyThrottle]
 
 
 class ContactUsView(ModelViewSet):
     queryset = ContactUs.objects.all()
     serializer_class = ContactUsSerializer
+    filterset_class = ContactUsFilter
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        rest_framework_filters.OrderingFilter,
+        rest_framework_filters.SearchFilter
+    )
+    search_fields = ['subject', 'email_to', 'message', ]
+    ordering_field = ['id', 'sent']
+    throttle_classes = [AnonCurrencyThrottle]
